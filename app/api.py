671edcd2ee97200d6d6612b8a57ab2c284e88d24
j0n3lson <620536+j0n3lson@users.noreply.github.com>
2023-04-23 03:15:24 +0000

    Adds user creation handling

--- app/api.py
+++ app/api.py
@@ -22,6 +22,7 @@ class UserType(str, Enum):
     def __str__(self) -> str:
         return self.value
 
+
 class User():
     '''A user in the system.'''
 
@@ -32,9 +33,20 @@ class User():
         self.created_on = datetime.now()
         self.api_key = api_key
 
+    def to_json(self):
+        '''Returns a JSON serializable object representing the user.'''
+        return {
+            'id': self.id,
+            'user': self.username,
+            'type': self.type,
+            'created_on': self.created_on.strftime('%m/%d/%Y %H:%M:%S')
+        }
+
 
 # TODO(j0n3lson): Don't store the admin's API key in code. Take it as a flag
 # saved in a github key.
+
+# TODO: Move these to UserManager
 api_keys = {
     'admin': ADMIN_API_KEY
 }
@@ -45,19 +57,47 @@ users = {
 
 
 class Users(Resource):
+
+    def get(self, username):
+        '''Get the user if they exist'''
+        self._abort_if_not_exists(username)
+        user = users.get(username)
+        return user.to_json()
+
+    def put(self, username):
+        '''Creates a new user if one doesn't already exist'''
+        if username in users:
+            user = users.get(username)
+            created_on = user.created_on.strftime(
+                '%m/%d/%Y %H:%M:%S')
+            abort(409, message=f'User {username} was created on {created_on}')
+        user = self._create_new_user(username)
+        return user.to_json()
+
+    def _create_new_user(self, username):
+        # TODO: Move this this to UserManager
+        api_key = self._generate_api_key()
+        id = self._get_next_id()
+        new_user = User(id=id, username=username,
+                        type=UserType.REGULAR, api_key=api_key)
+        users[username] = new_user
+        return new_user
+
+    def _generate_api_key(self):
+        # TODO: Move this this to UserManager
+        return random.choices(string.ascii_letters + string.digits, k=30)
+
+    def _get_next_id(self):
+        '''Returns the next available ID'''
+        # TODO: Move this this to UserManager and track next ID when users are
+        # created.
+        current_users = list(users.values())
+        current_users.sort(reverse=True, key=lambda user: user.id)
+        return current_users[0].id + 1
+
     def _abort_if_not_exists(self, username):
         if username not in users:
             abort(404, message=f'User {username} does not exist')
-
-    def get(self, username):
-        self._abort_if_not_exists(username)
-        user = users.get(username)
-        return {
-            'id': user.id,
-            'user': user.username,
-            'type': user.type,
-            'created_on': user.created_on.strftime('%m/%d/%Y %H:%M:%S')
-        }
 
 
 api.add_resource(Users, '/users/<username>')
